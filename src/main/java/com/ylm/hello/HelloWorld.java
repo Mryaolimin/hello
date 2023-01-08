b3070a560e018bbc49557fbe5a264e6f17a097e7
谦逊 <507509503@qq.com>
2023-01-09 00:12:40 +0800

    zs

--- src/main/java/com/ylm/hello/HelloWorld.java
+++ src/main/java/com/ylm/hello/HelloWorld.java
@@ -1,10 +1,10 @@
 package com.ylm.hello;
 
 /**
- * @author xb
+ * @author ylm
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World xb");
+        System.out.println("ylm");
     }
 }
